6099640107fb8a72050f731aa0c68e872179eb41
Sumit <sngsumit2003@gmail.com>
2024-12-16 19:59:46 +0530

    console fix

--- backend/server.mjs
+++ backend/server.mjs
@@ -31,5 +31,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+  console.log(`Expense app listening on port ${process.env.PORT}`);
 });
